451f5c19e85c6c9d97c78270333c6c8d96134a93
elnatan <elnatan@ravtech.co.il>
2020-01-16 19:36:11 +0200

    Initial Commit

---
+++ cat yearsTests/cat_yearsTests.swift
@@ -0,0 +1,34 @@
+//
+//  cat_yearsTests.swift
+//  cat yearsTests
+//
+//  Created by admin on 16/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import XCTest
+@testable import cat_years
+
+class cat_yearsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
