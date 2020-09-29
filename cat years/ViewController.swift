859a342eef85b0e9008d3ee9ead3050707ded63f
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 14:14:48 +0300

    first commit

--- cat years/ViewController.swift
+++ cat years/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var regYears: UITextField!
+    
+    @IBOutlet weak var catYearsAge: UILabel!
+    
+    @IBAction func calulateCatYears(_ sender: UIButton) {
+        
+        if let age = regYears.text{
+            
+            if let ageAsInt = Int(age){
+                print("age " + age)
+                print("ageAsInt " , ageAsInt)
+                let  ageInCatYears = ageAsInt * 7
+                print("ageInCatYears " , ageInCatYears)
+                catYearsAge.text = "Your cat is " + String(ageInCatYears) + " cat years old"
+
+            }
+        
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
-
